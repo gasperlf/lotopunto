786d6edef002e4eee3fc4a2ddb5b0d3de837bef6
Jhovanny <jhovannycanas@gmail.com>
2020-04-18 17:36:51 -0500

    start proyect

---
+++ ms-lotopunto/src/main/java/co/com/lotopunto/mslotopunto/MsLotopuntoApplication.java
@@ -0,0 +1,13 @@
+package co.com.lotopunto.mslotopunto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLotopuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLotopuntoApplication.class, args);
+	}
+
+}
---
+++ ms-lotopunto/src/test/java/co/com/lotopunto/mslotopunto/MsLotopuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.lotopunto.mslotopunto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLotopuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
